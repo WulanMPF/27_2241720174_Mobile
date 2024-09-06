40bbe8bd3f86507e79f0e71dc9c70d5b5b8acc8d
WulanMPF <wulanmaulidya1604@gmail.com>
2024-09-06 09:32:59 +0700

    Create Tugas.dart

---
+++ src/Jobsheet-2/Tugas.dart
@@ -0,0 +1,7 @@
+// SOAL 1 : Modifikasilah kode pada baris 3 di VS Code atau Editor Code favorit Anda berikut ini 
+//agar mendapatkan keluaran (output) sesuai yang diminta!
+void main() {
+  for (int i = 0; i < 10; i++) {
+    print('Nama saya adalah Fulan, sekarang berumur ${18 - i}');
+  }
+}
